ed7565953a53ea6fb09f13967f94bfeda807c10d
Kieran Hunt <kieran.hunt92@gmail.com>
2015-10-12 15:06:41 +0200

    🌻

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ for i in {1..20}
     do
         for book in $(find /mnt/datapool/kieran/books -name '*.txt');
         do
-            echo "cargo run $algorithm $book >> "/mnt/datapool/kieran/results/$algorithm-$i-`echo $book | rev | cut -d/ -f1 | rev`";"
-            #cargo run $algorithm $book >> "/mnt/datapool/kieran/results/$algorithm-$i-`echo $book | rev | cut -d/ -f1 | rev`";
+            echo "cargo run $algorithm $book >> "/mnt/datapool/kieran/results/books/$algorithm-$i-`echo $book | rev | cut -d/ -f1 | rev`";"
+            cargo run $algorithm $book >> "/mnt/datapool/kieran/results/books/$algorithm-$i-`echo $book | rev | cut -d/ -f1 | rev`";
         done
     done
 done
