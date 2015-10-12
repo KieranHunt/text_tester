d2016f2999e5d76dac91dba2b65faf740d71e24a
Kieran Hunt <kieran.hunt92@gmail.com>
2015-10-12 15:02:50 +0200

    Adding a simple testing script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cargo build > /dev/null;
+
+for i in {1..20}
+    do
+    for algorithm in BoyerMoore Horspool KnuthMorrisPratt Naive RabinKarp Bitap Bloom
+    do
+        for book in $(find /mnt/datapool/kieran/books -name '*.txt');
+        do
+            echo "cargo run $algorithm $book >> "/mnt/datapool/kieran/results/$algorithm-$i-`echo $book | rev | cut -d/ -f1 | rev`";"
+            #cargo run $algorithm $book >> "/mnt/datapool/kieran/results/$algorithm-$i-`echo $book | rev | cut -d/ -f1 | rev`";
+        done
+    done
+done
